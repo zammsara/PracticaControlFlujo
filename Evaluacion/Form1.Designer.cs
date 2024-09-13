90ac671477b32c2877d67314adac73cc77bf06a2
zammsara <saztaylor@uamv.edu.ni>
2024-09-12 20:27:04 -0600

    Completed PracticaControlFlujoExcercise

--- Evaluacion/Form1.Designer.cs
+++ Evaluacion/Form1.Designer.cs
@@ -28,73 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.tbNombres = new System.Windows.Forms.TextBox();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnMostrar = new System.Windows.Forms.Button();
             this.lblMensaje = new System.Windows.Forms.Label();
             this.tbResultadoNombres = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 33);
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 18);
+            this.label1.Size = new System.Drawing.Size(182, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre del estudiante:";
             // 
             // tbNombres
             // 
             this.tbNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbNombres.Location = new System.Drawing.Point(209, 33);
+            this.tbNombres.Location = new System.Drawing.Point(209, 47);
             this.tbNombres.Name = "tbNombres";
-            this.tbNombres.Size = new System.Drawing.Size(310, 24);
+            this.tbNombres.Size = new System.Drawing.Size(338, 24);
             this.tbNombres.TabIndex = 1;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(540, 19);
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAgregar.Image = ((System.Drawing.Image)(resources.GetObject("btnAgregar.Image")));
+            this.btnAgregar.Location = new System.Drawing.Point(553, 35);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(81, 61);
+            this.btnAgregar.Size = new System.Drawing.Size(55, 48);
             this.btnAgregar.TabIndex = 2;
-            this.btnAgregar.Text = "button1";
             this.btnAgregar.UseVisualStyleBackColor = true;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgg_Click);
             // 
             // btnMostrar
             // 
-            this.btnMostrar.Location = new System.Drawing.Point(174, 148);
+            this.btnMostrar.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(302, 171);
             this.btnMostrar.Name = "btnMostrar";
-            this.btnMostrar.Size = new System.Drawing.Size(214, 49);
+            this.btnMostrar.Size = new System.Drawing.Size(128, 34);
             this.btnMostrar.TabIndex = 3;
-            this.btnMostrar.Text = "button1";
+            this.btnMostrar.Text = "Mostrar";
             this.btnMostrar.UseVisualStyleBackColor = true;
             this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
             // 
             // lblMensaje
             // 
             this.lblMensaje.AutoSize = true;
-            this.lblMensaje.Location = new System.Drawing.Point(605, 135);
+            this.lblMensaje.Font = new System.Drawing.Font("SimSun", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblMensaje.Location = new System.Drawing.Point(206, 100);
             this.lblMensaje.Name = "lblMensaje";
-            this.lblMensaje.Size = new System.Drawing.Size(57, 13);
+            this.lblMensaje.Size = new System.Drawing.Size(224, 13);
             this.lblMensaje.TabIndex = 4;
-            this.lblMensaje.Text = "lblMensaje";
+            this.lblMensaje.Text = "Nombre de estudiante _ agregado";
+            this.lblMensaje.Click += new System.EventHandler(this.lblMensaje_Click);
             // 
             // tbResultadoNombres
             // 
-            this.tbResultadoNombres.Location = new System.Drawing.Point(103, 203);
+            this.tbResultadoNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbResultadoNombres.Location = new System.Drawing.Point(97, 211);
             this.tbResultadoNombres.Name = "tbResultadoNombres";
-            this.tbResultadoNombres.Size = new System.Drawing.Size(346, 20);
+            this.tbResultadoNombres.Size = new System.Drawing.Size(552, 24);
             this.tbResultadoNombres.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(263, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 34);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.tbResultadoNombres);
             this.Controls.Add(this.lblMensaje);
             this.Controls.Add(this.btnMostrar);
@@ -104,6 +123,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,6 +137,7 @@
         private System.Windows.Forms.Button btnMostrar;
         private System.Windows.Forms.Label lblMensaje;
         private System.Windows.Forms.TextBox tbResultadoNombres;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Evaluacion/Form1.cs
+++ Evaluacion/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Evaluacion
 {
@@ -24,23 +25,70 @@ namespace Evaluacion
 
         private void btnAgg_Click(object sender, EventArgs e)
         {
-        
+            //*Obtenemos el nombre ingresado y eliminamos espacios en blanco adicionales*//
+            string nombreCompleto = tbNombres.Text.Trim();
+
+            //* Validamos que el nombre no este vacio*//
+            if (string.IsNullOrEmpty(nombreCompleto))
+            {
+                MessageBox.Show("El nombre no puede estar vacio, ingresar nombres");
+                return;
+            }
+
+            //* Validamos que el nombre no contenga numeros *//
+            if (nombreCompleto.Any(char.IsDigit))
+            {
+                MessageBox.Show("El nombre no puede contener numeros");
+                return;
+            }
+
             if (contador < 25)
             {
+                //Guardar el nombre ingresado en el arreglo
                 estudiantes[contador] = tbNombres.Text;
                 contador++;
                 tbNombres.Clear();
 
-                lblMensaje.Text = $"Nombre de estudiante "
+                //opcional: mostrar mensaje que los nombres se han agg correctamente
+                lblMensaje.Text = $"Nombre de estudiante {contador} agregador correctamente";
+
+                //Si ya se ingreso el maximo 25, se desactivara el boton y se notificara
+                if (contador == 25)
+                {
+                    btnAgregar.Enabled = false;
+                    MessageBox.Show("Se ha alcanzado el maximo de nombres, 25");
+                    return;
+                }
             }
         }
 
         private void btnMostrar_Click(object sender, EventArgs e)
         {
+            tbResultadoNombres.Clear(); //Se limpia el cuadro de resultados
 
+            bool hayNombreLargos = false;
+
+            //Recorrre el arreglo en busca de los nombres con mas de 25 caracteres
+            for (int i = 0; i < estudiantes.Length; i++) 
+            {
+                if (estudiantes[i] != null && estudiantes[i].Length > 25) 
+                {
+                    tbResultadoNombres.AppendText(estudiantes[i] + Environment.NewLine + Environment.NewLine);
+
+                    hayNombreLargos |= true;
+                }
+            }   if (!hayNombreLargos) 
+                {
+                tbResultadoNombres.Text = "No hay nombres con mas de 25 caracteres";
+                }
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMensaje_Click(object sender, EventArgs e)
         {
 
         }
